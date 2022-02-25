3f68c3a223b8e48971e8cbac9b8376c59cbcbcec
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-02-25 20:39:15 +0530

    add datatypes : added datatypes

---
+++ datatypes/furniture.java
@@ -0,0 +1,30 @@
+class table 
+{
+public static void main(String []ar)
+{
+	byte NoOfLegs=4;
+	System.out.println(NoOfLegs);
+
+	short vehicalNo=7771;
+	System.out.println(vehicalNo);
+
+	int c=55555;
+	System.out.println(c);
+
+	long serNo=12345678901321214l;
+	System.out.println(serNo);
+
+	float a=1.5f;
+	System.out.println(a);
+
+	double b=1.8378562658762855;
+	System.out.println(b);
+	
+	char name='c';
+	System.out.println(name);
+
+	boolean wanted=true;
+	System.out.println(wanted);
+
+}
+}
