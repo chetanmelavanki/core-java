dc49d539da81bfbef2fed6ff2c82453475d3b9ac
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-02-25 20:44:07 +0530

    add return : added returnStatement

---
+++ returnStatements/Lap.java
@@ -0,0 +1,25 @@
+ class Lap 
+{
+    public static void main(String []a)
+    {
+        float result = Lap.pradeep();
+        System.out.println(result);
+			
+			int total = Lap.marks();
+			System.out.println(total);
+
+	
+		
+    }
+
+    static float pradeep()
+    {
+        float per=79.92f;
+        return per;
+    }
+	static int marks()
+	{
+			int total_marks=576;
+			return total_marks;
+	}
+}
