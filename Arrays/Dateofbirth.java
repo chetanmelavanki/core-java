b077a59cad3b9d4e6c88b3f273ff364f04012c74
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-02-25 20:33:06 +0530

    add Arrays : added Arrays

---
+++ Arrays/Dateofbirth.java
@@ -0,0 +1,30 @@
+class Dateofbirth
+{
+	static int[] dob={10022000,11022000,12022000,13022000,15061995,28091999,17122000,15092000,12091999,14102000,19022000,19091998,10022000,15121999,15032000};
+	public static void main(String []args)
+	{
+	
+	System.out.println(" Length= "+dob.length);
+	System.out.println(dob[0]);
+	System.out.println(dob[1]);
+	System.out.println(dob[2]);
+	System.out.println(dob[3]);
+	System.out.println(dob[4]);
+	System.out.println(dob[5]);
+	System.out.println(dob[6]);
+	System.out.println(dob[7]);
+	System.out.println(dob[8]);
+	System.out.println(dob[9]);
+	System.out.println(dob[10]);
+	System.out.println(dob[11]);
+	System.out.println(dob[12]);
+	System.out.println(dob[13]);
+	System.out.println(dob[14]);
+
+	date(10122000);
+	}
+static void date(int dateof)
+{
+	System.out.println(dateof);
+}
+}
