f89a3317c6a3795f52b80785f9fb656c7247914d
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-03-01 10:13:48 +0530

    Adding method amd method overloading

---
+++ MethodandMethodoverloading/src/libry.java
@@ -0,0 +1,17 @@
+
+
+public class libry 
+{
+	public static void main(String []args)
+	{
+		Library.bookname();
+		
+		Library.bookPrice(348.99);
+		
+		Library.bookAuthor("valmiki  ", " Dev publication");
+		
+		Library.bookPages(1289,"  Time New Roman");
+		
+		Library.book(348.99, 1289, "  Kannada", 'W');
+	}
+}
