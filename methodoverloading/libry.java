539b6559e6fa3a34e13337d6f705dee21607e2dc
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-03-01 17:53:29 +0530

    add methodoverloading

---
+++ methodoverloading/libry.java
@@ -0,0 +1,17 @@
+
+
+public class libry 
+{
+	public static void main(String []args)
+	{
+		Library.book("Ramayan ");
+		
+		Library.book(348.99);
+		
+		Library.book("valmiki  ", " Dev publication");
+		
+		Library.book(1289,"  Time New Roman");
+		
+		Library.book(348.99, 1289, "  Kannada", 'W');
+	}
+}
